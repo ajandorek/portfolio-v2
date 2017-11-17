cdb3eab4b415993128f716c38cfe75604e3a13e5
Alec Jandorek <ajandorek@gmail.com>
2017-11-17 14:33:29 -0600

    added styles, images, and ported over index page

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,41 +3,39 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-import './index.css'
+import './styles/index.scss'
 
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
+  <div>
+  <div className="top-bar">
+    <div className="top-bar-left">
+      <ul className="menu">
+        <Link to="/" className="menu-text logo">Alec Jandorek</Link>
+      </ul>
+    </div>
+    <div className="top-bar-right">
+      <ul className="menu">
+        <li>
+          <Link to="/"
+            className='currentPage'
+            activeClassName='currentPage'>Home</Link>
+        </li>
+        <li>
+          <Link to="/portfolio" activeClassName='currentPage'>Portfolio</Link>
+        </li>
+        <li>
+          <Link to="/contact_me" activeClassName='currentPage'>Contact</Link>
+        </li>
+      </ul>
     </div>
   </div>
+</div>
 )
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Alec Jandorek"
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
@@ -45,12 +43,12 @@ const TemplateWrapper = ({ children }) => (
     />
     <Header />
     <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
+      // style={{
+      //   margin: '0 auto',
+      //   maxWidth: 960,
+      //   padding: '0px 1.0875rem 1.45rem',
+      //   paddingTop: 0,
+      // }}
     >
       {children()}
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,47 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby-link';
+import FadeIn from 'react-fade-in';
+import FontAwesome from 'react-fontawesome';
 
 const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+  <FadeIn>
+    <div className='grid-container'>
+      <div className='header'>
+        <img src={require('../images/portfolio-image.png')} />
+        <h1>Alec Jandorek</h1>
+        <h3>Web Developer</h3>
+      </div>
+      <hr />
+      <p>Hi There I'm Alec! I'm a full stack web developer living in Austin, TX. I am a recent bootcamp graduate looking to break into the development field. I enjoy working on projects that incorporate React and Redux, and challenging myself to always be on the cutting edge with new technologies.</p>
+      <p>Feel free to take a look through my porfolio of applications I have created or collaborated on. Interested in chatting about an upcoming project? Drop me a line from one of the venues below and be sure to check out my resume <a href="/resume" target="_blank">here</a>. I look forward to hearing from you!</p>
+      <hr />
+      <div className='grid-container'>
+        <div className="icon-container">
+          <a href="https://www.linkedin.com/in/alecjandorek/" target="_blank">
+            <FontAwesome
+              className="icon"
+              name='linkedin'
+              size='2x'
+            />
+          </a>
+          <a href="https://github.com/ajandorek" target="_blank">
+            <FontAwesome
+              className="icon"
+              name='github'
+              size='2x'
+            />
+          </a>
+          <a href="mailto:ajandorek@gmail.com">
+            <FontAwesome
+              className="icon"
+              name='envelope'
+              size='2x'
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  </FadeIn>
+);
 
 export default IndexPage
