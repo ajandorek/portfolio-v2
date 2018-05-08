6670f8e6d9dc29212d657c8f1a68b7f79f513239
Alec Jandorek <ajandorek@gmail.com>
2018-05-08 09:52:57 -0500

    Added Sumo script

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'gatsby-link';
+import Helmet from 'react-helmet';
 
-import './styles/index.scss'
+import './styles/index.scss';
 
 const Header = () => (
   <div>
     <div className="top-bar" data-responsive-toggle="main-menu" data-hide-for="medium">
       <div className="top-bar-left">
         <ul className="menu">
-          <Link to="/" className="menu-text logo">Alec Jandorek</Link>
+          <Link to="/" className="menu-text logo">
+            Alec Jandorek
+          </Link>
         </ul>
       </div>
       <div className="top-bar-right">
@@ -28,7 +30,7 @@ const Header = () => (
       </div>
     </div>
   </div>
-)
+);
 
 const TemplateWrapper = ({ children }) => (
   <div>
@@ -36,18 +38,28 @@ const TemplateWrapper = ({ children }) => (
       title="Alec Jandorek"
       meta={[
         { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'keywords', content: 'sample, something' }
       ]}
     />
+    <Helmet>
+      <script async>
+        {(function(s, u, m, o, j, v) {
+          j = u.createElement(m);
+          v = u.getElementsByTagName(m)[0];
+          j.async = 1;
+          j.src = o;
+          j.dataset.sumoSiteId = '9fef5e2dd604421ac345b6ec2b471c842f55a7b59193a11ec7212f2096fcbb3b';
+          v.parentNode.insertBefore(j, v);
+        })(window, document, 'script', '//load.sumo.com/')}
+      </script>
+    </Helmet>
     <Header />
-    <div>
-      {children()}
-    </div>
+    <div>{children()}</div>
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-}
+  children: PropTypes.func
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,38 +5,36 @@ import FontAwesome from 'react-fontawesome';
 
 const IndexPage = () => (
   <FadeIn>
-    <div className='grid-container'>
-      <div className='header'>
+    <div className="grid-container">
+      <div className="header">
         <img src={require('../images/portfolio-image.png')} />
         <h1>Alec Jandorek</h1>
         <h3>Web Developer</h3>
       </div>
       <hr />
-      <p>Hi There I'm Alec! I'm a full stack web developer living in Austin, TX. I am a recent bootcamp graduate looking to break into the development field. I enjoy working on projects that incorporate React and Redux, and challenging myself to always be on the cutting edge with new technologies.</p>
-      <p>Feel free to take a look through my porfolio of applications I have created or collaborated on. Interested in chatting about an upcoming project? Drop me a line from one of the venues below and be sure to check out my resume <Link to="/resume">here</Link>. I look forward to hearing from you!</p>
+      <p>
+        Hi There I'm Alec! I'm a full stack web developer living in Austin, TX. I am a recent
+        bootcamp graduate looking to break into the development field. I enjoy working on projects
+        that incorporate React and Redux, and challenging myself to always be on the cutting edge
+        with new technologies.
+      </p>
+      <p>
+        Feel free to take a look through my porfolio of applications I have created or collaborated
+        on. Interested in chatting about an upcoming project? Drop me a line from one of the venues
+        below and be sure to check out my resume <Link to="/resume">here</Link>. I look forward to
+        hearing from you!
+      </p>
       <hr />
-      <div className='grid-container'>
+      <div className="grid-container">
         <div className="icon-container">
           <a href="https://www.linkedin.com/in/alecjandorek/" target="_blank">
-            <FontAwesome
-              className="icon"
-              name='linkedin'
-              size='2x'
-            />
+            <FontAwesome className="icon" name="linkedin" size="2x" />
           </a>
           <a href="https://github.com/ajandorek" target="_blank">
-            <FontAwesome
-              className="icon"
-              name='github'
-              size='2x'
-            />
+            <FontAwesome className="icon" name="github" size="2x" />
           </a>
           <a href="mailto:ajandorek@gmail.com">
-            <FontAwesome
-              className="icon"
-              name='envelope'
-              size='2x'
-            />
+            <FontAwesome className="icon" name="envelope" size="2x" />
           </a>
         </div>
       </div>
@@ -44,4 +42,4 @@ const IndexPage = () => (
   </FadeIn>
 );
 
-export default IndexPage
+export default IndexPage;
